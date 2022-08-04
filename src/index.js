0a56ed18907bbb27231094530acb39dc9e9c7640
milana-bilych <akimmilana@gmail.com>
2022-08-04 21:10:05 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let massive = expr.split('');
+    let arr1 = [];
+
+    for (let i = 0; i < massive.length; i = i + 10) {
+        arr1.push(massive.slice(i, i + 10));
+    }
+
+    let arr2 = [];
+    arr1.forEach(item => {
+        symbol = ''
+
+        for (let i = 9; i > 0; i = i - 2) {
+            if (item[i - 1] + item[i] == '10') {
+                symbol = '.' + symbol
+            }
+            if (item[i - 1] + item[i] == '11') {
+                symbol = '-' + symbol
+            }
+            if (item[i - 1] + item[i] == '**') {
+                symbol = '' + symbol
+            }
+        }
+        arr2.push(symbol)
+    })
+
+    total = '';
+    arr2.forEach(symbol => {
+        if (!MORSE_TABLE[symbol]) {
+            total += ' '
+        } else total += MORSE_TABLE[symbol];
+    })
+    return total
 }
 
 module.exports = {
